ca04808700d3e5d6fe3c502c9d8f0794a0d03496
Alex <alex-007.88@mail.ru>
2021-08-24 15:56:44 +0300

    обнулил проэкт

--- src/app/direktiv.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DirektivDirective } from './direktiv.directive';
-
-describe('DirektivDirective', () => {
-  it('should create an instance', () => {
-    const directive = new DirektivDirective();
-    expect(directive).toBeTruthy();
-  });
-});
