2cf71d515a62941ed21b1edbe619af7e5fff64fb
Alex <alex-007.88@mail.ru>
2021-08-24 13:40:08 +0300

    начальный commit

---
+++ src/app/direktiv.directive.spec.ts
@@ -0,0 +1,8 @@
+import { DirektivDirective } from './direktiv.directive';
+
+describe('DirektivDirective', () => {
+  it('should create an instance', () => {
+    const directive = new DirektivDirective();
+    expect(directive).toBeTruthy();
+  });
+});
