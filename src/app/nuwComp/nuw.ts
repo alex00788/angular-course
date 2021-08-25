73604b1a26e3935641590057cad435a9b3944ee2
Alex <alex-007.88@mail.ru>
2021-08-25 22:22:55 +0300

    с помощью *ngIf показываю один либо др текст и тоже с img только img переключаються 1 раз

--- src/app/nuwComp/nuw.ts
+++ src/app/nuwComp/nuw.ts
@@ -7,5 +7,9 @@ import {Component} from '@angular/core';
 })
 export class NuwComponent {
 
-    back = false
+    perem = false
+    img = 'https://im0-tub-ru.yandex.net/i?id=7259a2a403c87fb68bba7f5b84683872-l&n=13'
+    imgg= 'https://www.fonstola.ru/download.php?file=201111/1680x1050/fonstola.ru-55607.jpg'
+
+
 }
