cc4bebd3fbd408eb3d4feee2603675d7bb8ac684
Alex <alex-007.88@mail.ru>
2021-08-25 09:59:22 +0300

    прописал setTimout заменяя одно фото другим через 3 сек

--- src/app/nuwComp/nuw.ts
+++ src/app/nuwComp/nuw.ts
@@ -9,4 +9,10 @@ export class NuwComponent {
 per = 'любой текст или заголовок, или число, или сумма чисел, или массив...... '
 
     img = 'https://pbs.twimg.com/media/C2KFY4tWQAIyrRa.jpg'
+
+    constructor() {
+    setTimeout(() => {
+        this.img = 'https://im0-tub-ru.yandex.net/i?id=7259898a768a5b11df3530d396b6373c-l&ref=rim&n=13&w=1080&h=1080'
+    }, 3000)
+    }
 }
