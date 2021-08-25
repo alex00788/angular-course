3d88b99a59947bf515af8693718ec10f569ea117
Alex <alex-007.88@mail.ru>
2021-08-25 09:41:53 +0300

    через интерполяцию вывел фото , добавил стилей

--- src/app/nuwComp/nuw.ts
+++ src/app/nuwComp/nuw.ts
@@ -7,4 +7,6 @@ import {Component} from '@angular/core';
 })
 export class NuwComponent {
 per = 'любой текст или заголовок, или число, или сумма чисел, или массив...... '
+
+    img = 'https://pbs.twimg.com/media/C2KFY4tWQAIyrRa.jpg'
 }
