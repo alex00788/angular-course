0ddbacd7f3f414286c1fc250e756b86bd47f36a7
Alex <alex-007.88@mail.ru>
2021-08-26 12:24:50 +0300

    создал массив и вывел его в шаблоне

--- src/app/src/spis/spis.component.ts
+++ src/app/src/spis/spis.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Spis {
+  surname: string
+  name: string
+
+}
+
 @Component({
   selector: 'app-spis',
   templateUrl: './spis.component.html',
@@ -8,7 +14,10 @@ import { Component, OnInit } from '@angular/core';
 export class SpisComponent implements OnInit {
 
   constructor() { }
-
+massiv: Spis [] = [
+  {surname: 'Фоничев', name: 'Александр'},
+  {surname: 'Popov', name: 'Александр'}
+]
   ngOnInit() {
   }
 
