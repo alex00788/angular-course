8fa7cf461946c9eb8534f777120f43c283d3fe90
Alex <alex-007.88@mail.ru>
2021-08-26 11:58:29 +0300

    создал 2 нов компанента и прописал их в главном

---
+++ src/app/src/spis/spis.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-spis',
+  templateUrl: './spis.component.html',
+  styleUrls: ['./spis.component.scss']
+})
+export class SpisComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
