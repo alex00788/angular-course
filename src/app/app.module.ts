0fcae9b442c891bb2d18986a210912717108edff
Alex <alex-007.88@mail.ru>
2021-08-27 12:19:31 +0300

    создал еще одну директиву с помощью cli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
 import {StyleDir} from './Style.dir';
+import { NDirDirective } from './n-dir.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StyleDir
+    StyleDir,
+    NDirDirective
   ],
   imports: [
     BrowserModule,
---
+++ src/app/n-dir.directive.spec.ts
@@ -0,0 +1,8 @@
+import { NDirDirective } from './n-dir.directive';
+
+describe('NDirDirective', () => {
+  it('should create an instance', () => {
+    const directive = new NDirDirective();
+    expect(directive).toBeTruthy();
+  });
+});
