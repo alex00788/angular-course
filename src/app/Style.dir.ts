ead0b831d14d6bc14101695a7b7f0aadba82f559
Alex <alex-007.88@mail.ru>
2021-08-27 15:48:02 +0300

    передал объект и описал его ... получилась стильная рамка

--- src/app/Style.dir.ts
+++ src/app/Style.dir.ts
@@ -6,6 +6,7 @@ import {Directive, ElementRef, HostListener, Input, Renderer2} from '@angular/co
 export class StyleDir {
     @Input('appStyle') color: string = 'blue'
     @Input() fontWeight = 'normal'
+    @Input() dSt : {border?: string, fontWeight?: string, borderRadius?: string}
 
 
     constructor(private per1: ElementRef, private per2: Renderer2) {
@@ -14,9 +15,15 @@ export class StyleDir {
     @HostListener('mouseenter') onEnter () {
         this.per2.setStyle(this.per1.nativeElement, 'color', this.color)
         this.per2.setStyle(this.per1.nativeElement, 'fontWeight', this.fontWeight)
+        this.per2.setStyle(this.per1.nativeElement, 'fontWeight', this.dSt)
+        this.per2.setStyle(this.per1.nativeElement, 'border', this.dSt.border)
+        this.per2.setStyle(this.per1.nativeElement, 'borderRadius', this.dSt.borderRadius)
     }
     @HostListener('mouseleave') onLeave () {
         this.per2.setStyle(this.per1.nativeElement, 'color', 'blue')
         this.per2.setStyle(this.per1.nativeElement, 'fontWeight', null)
+        this.per2.setStyle(this.per1.nativeElement, 'fontWeight', null)
+        this.per2.setStyle(this.per1.nativeElement, 'border', null)
+        this.per2.setStyle(this.per1.nativeElement, 'borderRadius', null)
     }
 }
