fe62381dac8c77efa63a5472dfa04228dcdf817f
Alex <alex-007.88@mail.ru>
2021-08-27 12:20:47 +0300

    подготовил проэкт очистил от лишних директив

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
 import {StyleDir} from './Style.dir';
-import { NDirDirective } from './n-dir.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StyleDir,
-    NDirDirective
+    StyleDir
   ],
   imports: [
     BrowserModule,
--- src/app/n-dir.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { NDirDirective } from './n-dir.directive';
-
-describe('NDirDirective', () => {
-  it('should create an instance', () => {
-    const directive = new NDirDirective();
-    expect(directive).toBeTruthy();
-  });
-});
