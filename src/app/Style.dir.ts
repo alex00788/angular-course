5adc0c914dc6aad830a96b9317d49611ef1ccd40
Alex <alex-007.88@mail.ru>
2021-08-27 15:12:09 +0300

    поменял код передал параметры через Input

--- src/app/Style.dir.ts
+++ src/app/Style.dir.ts
@@ -1,19 +1,20 @@
-import {Directive, ElementRef, HostListener, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostListener, Input, Renderer2} from '@angular/core';
 
 @Directive({
     selector: '[appStyle]'
 })
 export class StyleDir {
+    @Input('appStyle') color: string = 'blue'
     constructor(private per1: ElementRef, private per2: Renderer2) {
 
     }
     @HostListener('click', ['$event']) onCli(tip: Event) {
-        this.per2.setStyle(this.per1.nativeElement, 'color', 'blue')
+        this.per2.setStyle(this.per1.nativeElement, 'color', this.color)
     }
     @HostListener('mouseenter') onEnter () {
-        this.per2.setStyle(this.per1.nativeElement, 'color', 'blue')
+        this.per2.setStyle(this.per1.nativeElement, 'color', this.color)
     }
     @HostListener('mouseleave') onLeave () {
-        this.per2.setStyle(this.per1.nativeElement, 'color', 'red')
+        this.per2.setStyle(this.per1.nativeElement, 'color', null)
     }
 }
