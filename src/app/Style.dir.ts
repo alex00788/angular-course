8e3b619ff00177106d59b5f5217b5c45a6a91fc8
Alex <alex-007.88@mail.ru>
2021-08-27 14:26:59 +0300

    реализовал выделение при наведении

--- src/app/Style.dir.ts
+++ src/app/Style.dir.ts
@@ -4,10 +4,16 @@ import {Directive, ElementRef, HostListener, Renderer2} from '@angular/core';
     selector: '[appStyle]'
 })
 export class StyleDir {
-    constructor(private per: ElementRef, private perT: Renderer2) {
+    constructor(private per1: ElementRef, private per2: Renderer2) {
 
     }
     @HostListener('click', ['$event']) onCli(tip: Event) {
-        this.perT.setStyle(this.per.nativeElement, 'color', 'blue')
+        this.per2.setStyle(this.per1.nativeElement, 'color', 'blue')
+    }
+    @HostListener('mouseenter') onEnter () {
+        this.per2.setStyle(this.per1.nativeElement, 'color', 'blue')
+    }
+    @HostListener('mouseleave') onLeave () {
+        this.per2.setStyle(this.per1.nativeElement, 'color', 'red')
     }
 }
