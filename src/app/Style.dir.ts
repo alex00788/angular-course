487df972029e033136ca0209b8e54b90ab4d1dea
Alex <alex-007.88@mail.ru>
2021-08-29 13:10:14 +0300

    подключил @HostBinding описал как работает

--- src/app/Style.dir.ts
+++ src/app/Style.dir.ts
@@ -1,4 +1,4 @@
-import {Directive, ElementRef, HostListener, Input, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostBinding, HostListener, Input, Renderer2} from '@angular/core';
 
 @Directive({
     selector: '[appStyle]'
@@ -7,20 +7,23 @@ export class StyleDir {
     @Input('appStyle') color: string = 'blue'
     @Input() dSt : {border?: string, fontWeight?: string, borderRadius?: string}
 
+    @HostBinding('style.color') perColor = null
 
     constructor(private per1: ElementRef, private per2: Renderer2) {
 
     }
     @HostListener('mouseenter') onEnter () {
-        this.per2.setStyle(this.per1.nativeElement, 'color', this.color)
-        this.per2.setStyle(this.per1.nativeElement, 'fontWeight', this.dSt)
-        this.per2.setStyle(this.per1.nativeElement, 'border', this.dSt.border)
-        this.per2.setStyle(this.per1.nativeElement, 'borderRadius', this.dSt.borderRadius)
+        this.perColor = this.color
+        // this.per2.setStyle(this.per1.nativeElement, 'color', this.color)
+        // this.per2.setStyle(this.per1.nativeElement, 'fontWeight', this.dSt)
+        // this.per2.setStyle(this.per1.nativeElement, 'border', this.dSt.border)
+        // this.per2.setStyle(this.per1.nativeElement, 'borderRadius', this.dSt.borderRadius)
     }
     @HostListener('mouseleave') onLeave () {
-        this.per2.setStyle(this.per1.nativeElement, 'color', 'blue')
-        this.per2.setStyle(this.per1.nativeElement, 'fontWeight', null)
-        this.per2.setStyle(this.per1.nativeElement, 'border', null)
-        this.per2.setStyle(this.per1.nativeElement, 'borderRadius', null)
+        this.perColor = null
+        // this.per2.setStyle(this.per1.nativeElement, 'color', 'blue')
+        // this.per2.setStyle(this.per1.nativeElement, 'fontWeight', null)
+        // this.per2.setStyle(this.per1.nativeElement, 'border', null)
+        // this.per2.setStyle(this.per1.nativeElement, 'borderRadius', null)
     }
 }
