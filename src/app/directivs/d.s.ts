a2ffaabaf876eb9a1ff226be7140abedd22fa35d
Alex <alex-007.88@mail.ru>
2021-08-30 11:22:06 +0300

    попробовал применить разные стили

--- src/app/directivs/d.s.ts
+++ src/app/directivs/d.s.ts
@@ -6,12 +6,8 @@ import {Directive, ElementRef, HostListener, Renderer2} from '@angular/core';
 export class Ds {
 
     constructor(private perNel: ElementRef, private perRend: Renderer2) {
+        this.perRend.setStyle(this.perNel.nativeElement, 'color', 'orange');
+        this.perRend.setStyle(this.perNel.nativeElement, 'fontWeight', 'bold')
+        this.perRend.setStyle(this.perNel.nativeElement, 'font-size', '150px')
     }
-
-    @HostListener ('mouseenter') metEnter () {
-        this.perRend.setStyle(this.perNel.nativeElement, 'color', 'blue');
-    }@HostListener ('mouseleave') metleave () {
-        this.perRend.setStyle(this.perNel.nativeElement, 'color', null);
-    }
-
 }
