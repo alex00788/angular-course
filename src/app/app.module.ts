15c1a849132a899dd25d24a33c37a63084f68990
Alex <alex-007.88@mail.ru>
2021-08-30 22:40:57 +0300

    создал пайп в ручную

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
+import {MBPipe} from './pipes/m-b.pipe';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    MBPipe
    ],
   imports: [
     BrowserModule,
