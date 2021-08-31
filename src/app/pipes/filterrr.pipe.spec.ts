e6b5a16c2432917911948b5a6ef5af3245a11397
Alex <alex-007.88@mail.ru>
2021-08-31 10:43:30 +0300

    разобрал как создать пап фильтрующий список

---
+++ src/app/pipes/filterrr.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { FilterrrPipe } from './filterrr.pipe';
+
+describe('FilterrrPipe', () => {
+  it('create an instance', () => {
+    const pipe = new FilterrrPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
