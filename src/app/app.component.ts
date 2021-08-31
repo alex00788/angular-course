daa553f10243b3e5da791e43bd2fe36b5dfff6c2
Alex <alex-007.88@mail.ru>
2021-08-31 17:30:31 +0300

    сделал 2 счетчика с разной областью видимости локально и глобально

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,19 @@
 import {Component} from '@angular/core';
 import {NS} from './ servises/n.s';
+import {LocSService} from './ servises/loc-s.service';
 
 
 @Component({
   selector: 'app-sky',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [LocSService]
 })
 
 export class AppComponent {
-  constructor(private perPS: NS) {
+  constructor(
+      private perPS: NS,
+      private lokSServ: LocSService
+  ) {
   }
 }
