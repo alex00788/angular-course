141fd71ee14c78d8becf4cd93e98caa6e8174f69
Alex <alex-007.88@mail.ru>
2021-08-31 15:42:45 +0300

    создал сервис в ручную прописал его в модуле и логику простую прописал

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {NS} from './ servises/n.s';
 
 
 @Component({
@@ -8,5 +9,6 @@ import {Component} from '@angular/core';
 })
 
 export class AppComponent {
-
+  constructor(private perPS: NS) {
+  }
 }
