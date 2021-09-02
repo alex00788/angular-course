530a57c96c2a7e880212d68129b6eb595809b9a4
Alex <alex-007.88@mail.ru>
2021-09-02 14:45:01 +0300

    дополнение к уроку

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,41 +10,54 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
 })
 
 export class AppComponent implements OnInit {
-  perForm: FormGroup
+    perForm: FormGroup
 
-  ngOnInit() {
-    this.perForm = new FormGroup({
-      email:  new FormControl('', [
-          Validators.email,
-          Validators.required]),
-        password:  new FormControl(null, [
-          Validators.required,
-          Validators.minLength(4)]),
+    ngOnInit() {
+        this.perForm = new FormGroup({
+            email: new FormControl('', [
+                Validators.email,
+                Validators.required]),
+            password: new FormControl(null, [
+                Validators.required,
+                Validators.minLength(4)]),
 
-        address: new FormGroup({
-            strana: new FormControl('by'),
-            gorod: new FormControl('Moscow', Validators.required)
+            address: new FormGroup({
+                country: new FormControl('ru'),
+                city: new FormControl('', Validators.required)
+            })
         })
-    })
-  }
+    }
 
-  metodSubmit() {
-      if (this.perForm.valid) {
-          console.log('perForm', this.perForm);
-          const fp = (this.perForm.value)
-          console.log('fp', fp);
-      }
-  }
-
-    setStrana() {
-        const perSetGorod = {
-            ru: 'Moscow',
-            by: 'Минск'
+    metodSubmit() {
+        if (this.perForm.valid) {
+            console.log('perForm', this.perForm);
+            const fp = (this.perForm.value)
+            console.log('fp', fp);
         }
-        const perGorodKluch = this.perForm.get('address').get('strana').value
-        const perGorod = perSetGorod [perGorodKluch]
-        console.log(perGorod);
-        this.perForm.patchValue({
-            address: {gorod: perGorod}})
+    }
+
+    metod() {
+        return [
+            { abr: 'ru', name: 'Россия', capital: 'Москва'},
+            { abr: 'ua', name: 'Украина', capital: 'Киев'},
+            { abr: 'by', name: 'Беларусь', capital: 'Минск'}
+        ]
+    }
+    setStrana() {
+        // const perSetGorod = {
+        //     ru: 'Moscow',
+        //     by: 'Минск'
+        // }
+        const perKluch = this.perForm.get('address').get('country').value
+        const perGorod = this.metod ()
+            .filter( ( perAny1 ) => perAny1.abr === perKluch)
+            .map( perAny22 => perAny22.capital )
+
+        this.perForm.patchValue({address: { city: perGorod + '!!!!' }})
+    }
+
+    changeGor() {
+        this.setStrana();
     }
 }
+
