e0824040a7bcc735208b47cd1134a871aed6858e
Alex <alex-007.88@mail.ru>
2021-09-03 08:34:32 +0300

    ...

--- src/app/my.validators.ts
+++ src/app/my.validators.ts
@@ -1,7 +1,7 @@
 import {FormControl} from '@angular/forms';
 
 export class MyValidators {
-    static bedEmail (control: FormControl) : {[key: string]: boolean}{
+    static bedEmail (control: FormControl) : {[key: string]: boolean} {
        if (['bed@email.ru' , 'b@mail.ru'].includes(control.value)) {
            return {'bedEmail':true}
        }
