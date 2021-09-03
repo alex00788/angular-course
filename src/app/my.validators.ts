1442f2a62b9fffd6d6c79fb138f0b2a71f120788
Alex <alex-007.88@mail.ru>
2021-09-03 09:17:13 +0300

    асинхроный валидатор для проверки данных на сервере

--- src/app/my.validators.ts
+++ src/app/my.validators.ts
@@ -1,4 +1,5 @@
 import {FormControl} from '@angular/forms';
+import {Observable} from 'rxjs';
 
 export class MyValidators {
     static bedEmail (control: FormControl) : {[key: string]: boolean}{
@@ -6,5 +7,18 @@ export class MyValidators {
            return {'bedEmail':true}
        }
         return  null
+    }
+
+    static uniqEm (control: FormControl) : Promise<any> | Observable<any> {
+        return new Promise(resolve => {
+            setTimeout( () => {
+                if (control.value === 'aj@mail.ru') {
+                    resolve ({
+                        uniqEm: true })
+                } else {
+                    resolve: null
+                }
+            }, 1000)
+        })
     }
  }
